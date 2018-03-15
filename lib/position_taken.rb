e2476939875e7b592d8a1c036010ebc78cdad2a2
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-03-15 22:01:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,8 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  return !board[index].empty?
+  if board[index].empty?
+    return FALSE
+  else
+    return TRUE
+  end
 end
